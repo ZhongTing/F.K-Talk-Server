1ed40cb678e7906de735e1702686f74ddd99bb18
KemingChen <believe75467@gmail.com>
2014-05-09 13:02:38 +0800

    log to log.txt & show on the console minimize

--- model/fkmqtt.js
+++ model/fkmqtt.js
@@ -1,4 +1,5 @@
 var mqtt = require('mqtt')
+var fs = require('fs')
 var client = mqtt.createClient(1883, '140.124.183.138', {clientId: "FKServer", clean: false});
 var prefix = "FK"
 var connectoin = require("./db").connectoin;
@@ -9,7 +10,10 @@ function publish(topic, message)
 	client.publish(prefix.concat(topic), message, {qos:1, retain:false});
 	console.log("mqtt---------");
 	console.log(prefix.concat(topic));
-	console.log(message);
+	console.log(message.length > 1000 ? message.substr(0, 50) + "...(size: " + message.length + ")" : message);
+	fs.writeFile('log.txt', "\n " + message, {flag: "a"}, function (err) {
+		if (err) throw err;
+	});
 }
 
 function action(target, action, data)
